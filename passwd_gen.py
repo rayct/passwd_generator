9a957f24581ce5074025af739b908b88bb224146
rayct <rayct@icloud.com>
2023-12-03 20:36:52 +0000

    Content Revision: Dec 3rd, 2023 - Added Button spacing

--- passwd_gen.py
+++ passwd_gen.py
@@ -5,8 +5,16 @@ from tkinter import messagebox
 from tkinter import filedialog
 from fpdf import FPDF  # Required for PDF export
 
+password_generated = False
+
 def generate_password():
-    password_length = int(length_entry.get())
+    global password_generated
+    password_length = length_entry.get()
+    if not password_length:
+        messagebox.showwarning("Warning", "Please input Password Length")
+        return
+    
+    password_length = int(password_length)
     complexity = ""
     if uppercase_var.get():
         complexity += string.ascii_uppercase
@@ -22,8 +30,14 @@ def generate_password():
     password_display.delete(1.0, tk.END)
     password_display.insert(tk.END, password)
     password_display.config(state=tk.DISABLED)
+    password_generated = True
 
 def save_to_file():
+    global password_generated
+    if not password_generated:
+        messagebox.showerror("Error", "No password generated yet!")
+        return
+    
     password = password_display.get(1.0, tk.END)
     file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt"), ("PDF Files", "*.pdf")])
     
@@ -40,18 +54,29 @@ def save_to_file():
                 file.write(password)
 
 def copy_to_clipboard():
+    global password_generated
+    if not password_generated:
+        messagebox.showerror("Error", "No password generated yet!")
+        return
+    
     password = password_display.get(1.0, tk.END)
     root.clipboard_clear()
     root.clipboard_append(password)
     messagebox.showinfo("Copy to Clipboard", "Password copied to clipboard!")
 
 def clear_password():
+    global password_generated
+    if not password_generated:
+        messagebox.showerror("Error", "No password generated yet!")
+        return
+    
     password_display.config(state=tk.NORMAL)
     password_display.delete(1.0, tk.END)
     password_display.config(state=tk.DISABLED)
+    password_generated = False
 
 def show_about():
-    messagebox.showinfo("About", "Password Generator App\nAuthor: Raymond C. Turner.\nApp Version 1.1.0")
+    messagebox.showinfo("About", "Password Generator App\nCreated with Tkinter.\nApp Version 1.0.1-beta")
 
 root = tk.Tk()
 root.title("Password Generator")
@@ -114,16 +139,16 @@ password_display.pack()
 
 # UI Buttons for Save to File, Copy to Clipboard, Clear Password
 save_button = tk.Button(root, text="Save to File", command=save_to_file)
-save_button.pack()
+save_button.pack(pady=5)
 
 copy_clipboard_button = tk.Button(root, text="Copy to Clipboard", command=copy_to_clipboard)
-copy_clipboard_button.pack()
+copy_clipboard_button.pack(pady=5)
 
 clear_button = tk.Button(root, text="Clear Password", command=clear_password)
-clear_button.pack()
+clear_button.pack(pady=5)
 
 # Interface label at bottom right
-interface_label = tk.Label(root, text="codestak.io", fg="gray")
+interface_label = tk.Label(root, text="codestak.io-beta", fg="gray")
 interface_label.pack(side=tk.RIGHT, anchor=tk.SE)
 
 root.mainloop()
